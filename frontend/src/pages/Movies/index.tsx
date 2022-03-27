ffd1e9387f28a881e0c82cb52ac427d558c579d0
Victor Abreu <vlabreu@outlook.com>
2022-03-26 21:25:08 -0300

    Ajuste link pagina Movies

--- frontend/src/pages/Movies/index.tsx
+++ frontend/src/pages/Movies/index.tsx
@@ -1,11 +1,16 @@
-import "./styles.css";
+import './styles.css';
+import { Link } from 'react-router-dom';
 
 const Movies = () => {
   return (
     <div className="movies-container">
       <h2>Tela listagem de filmes</h2>
-      <a href="/movies/1">Acessar/movies/1</a>
-      <a href="/movies/2">Acessar/movies/2</a>
+      <Link to="/movies/1">
+        <h5>Acessar/movies/1</h5>
+      </Link>
+      <Link to="/movies/2">
+        <h5>Acessar/movies/2</h5>
+      </Link>
     </div>
   );
 };
